ec4affe25e5912b3fb819931677826926c1416de
yyamakawa41 <yohsukeyamakawa@Yohsukes-MacBook-Air.local>
2015-10-15 19:13:07 -0400

    winner bust loser
    script updates to wins and losses
    reset

--- script.js
+++ script.js
@@ -54,6 +54,9 @@ function shuffleDeck(){
 		var total = 0;
 		for (i=0; i<hand.length; i++){
 			var cardValue = Number(hand[i].slice(0, -1));
+			if (cardValue >10){
+				cardValue=10
+			}
 			total = total + cardValue; //total += cardValue;
 		}
 		var idWhoToGet = who + '-total';
@@ -62,10 +65,13 @@ function shuffleDeck(){
 		if(total > 21){
 			bust(who);
 			console.log(total);
+			checkWin();
 		}
+		return total;
 	}	
 
 	function deal(){
+		reset()
 		deck = shuffleDeck();
 		playerHand = [ deck[0], deck[2] ];
 		dealerHand = [ deck[1], deck[3] ];
@@ -94,8 +100,61 @@ function shuffleDeck(){
 		calculateTotal(playerHand, 'player');
 	}	
 
-	function stand(){	
-	
+	function checkWin(){
+		var playerTotal = Number(document.getElementById('player-total').innerHTML);
+		var dealerTotal = Number(document.getElementById('dealer-total').innerHTML);
+		var winner
+		if (((playerTotal > dealerTotal) && (playerTotal <= 21)) || ((playerTotal <= 21) && (dealerTotal > 21))){
+			winner = 'player';
+		} else if ((playerTotal===dealerTotal)&&(playerTotal<=21)){
+			winner='tie'
+		}
+		else{
+			winner = 'dealer';
+		}
+		console.log("winner: " + winner);
+		if(winner === 'player'){
+			document.getElementById('message').innerHTML="You Win!"
+		}
+		if(winner === 'tie'){
+			document.getElementById('message').innerHTML="It's a push!"
+		}if(winner === 'dealer'){
+			document.getElementById('message').innerHTML="Dealer Wins!"
+		}
+		document.getElementById('message').innerHTML = "Wanna play again?";
+	}
 
+	function reset(){
+		deck = [];
+	 	placeInDeck = 0;
+ 		playerTotalCards = 2;
+ 		dealerTotalCards = 2;
+ 		playerHand;
+ 		dealerHand;
+		document.getElementById('message').innerHTML = "";
+
+		var cards = document.getElementsByClassName('card');
+		for( i=0; i < cards.length; i++){
+			cards[i].className = cards[i].className + " empty";
+			cards.innerHTML = "";
+		}
+	}
+
+	function stand(){	
+		var dealerHas = calculateTotal(dealerHand, 'dealer');
+		// var dealerHas = Number(document.getElementById('dealer-total').innerHTML);
+		var slot;
+		while (dealerHas < 17){
+			if (dealerTotalCards === 2){slot = "three";}
+			else if (dealerTotalCards === 3){slot = "four";}
+			else if (dealerTotalCards === 4){slot = "five";}
+			else if (dealerTotalCards === 5){slot = "six";}	
+			placeCard(deck[placeInDeck], 'dealer', slot);
+			dealerHand.push(deck[placeInDeck]);
+			dealerHas = calculateTotal(dealerHand, 'dealer');
+			placeInDeck++;
+			dealerTotalCards++;
+		}
+		checkWin();
 	}	
 
